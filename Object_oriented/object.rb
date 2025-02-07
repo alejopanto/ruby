127af38a924dca96b669a13e5e4c772d6e9d0f4c
alejopanto <jose.pantoja@testu.co>
2025-02-07 11:44:03 -0500

    object

---
+++ Object_oriented/object.rb
@@ -0,0 +1,11 @@
+class Book
+  def initialize(title:, author:, pub_year:)
+    @title = title
+    @author = author
+    @pub_year = pub_year
+  end
+end
+
+b = Book.new(, pub_year: "2020", title: "code", author: "fulano")
+
+p b
