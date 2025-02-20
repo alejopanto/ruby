3c38eef647cbb83cbe3c30897235029769ace5d3
alejopanto <jose.pantoja@testu.co>
2025-02-20 15:55:52 -0500

    tipos de datos 3

---
+++ Course_ruby/tipo_datos3.rb
@@ -0,0 +1,9 @@
+nombre = "simon"
+nombre_2 = "simon"
+nombre.object_id # Return a ramdon ID
+
+#Symbol
+color = :rojo
+color_2 = :rojo
+color.object_id # has the same id to color_2 beacuse is the same symbol
+:rojo.class #Symbol (is not a string), use the same memory, is the mix of String and Number
