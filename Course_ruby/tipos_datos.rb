d3520a61aa943688f78ddfa650ba7418530ef662
alejopanto <jose.pantoja@testu.co>
2025-02-20 11:22:02 -0500

    Tipos de datos

---
+++ Course_ruby/tipos_datos.rb
@@ -0,0 +1,11 @@
+x = 5
+x.class #Show the class (Integer, float, array, object, etc)
+x.methods #Show methods
+x.odd? #Show true if is Impair
+x.even? #Show true if is Pair
++ - / * % ** #Mas, menos, dividido, multiplicado, modulo, elevado
+
+4/5 #show 0 (Use Integer return Integer)
+4/5.0 #Show 0.8 (Use Float return Float)
+
+100**100 # Return Integer (the long of the number dont matter)
