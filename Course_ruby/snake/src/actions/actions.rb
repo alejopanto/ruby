a846dce659e19708257f26f2378dd93cf65c8177
alejopanto <jose.pantoja@testu.co>
2025-03-26 15:41:57 -0500

    movimientos snake

---
+++ Course_ruby/snake/src/actions/actions.rb
@@ -0,0 +1,68 @@
+module Actions
+  def self.move_snake(state)
+    next_direction = state.next_direction
+    next_position = calc_next_position(state)
+
+    if position_is_valid?(state, next_position)
+      move_snake_to(state, next_direction)
+    else
+    end_game(state)
+    end
+  end
+
+  private
+
+  def calc_next_position(state)
+
+    curr_position = state.snake.position.first
+
+    case state.next_direction
+    when UP
+      return Model::Coord.new(
+        curr_position.row - 1,
+        curr_position.col
+      )
+
+    when RIGHT
+      return Model::Coord.new(
+        curr_position.row,
+        curr_position.col + 1
+      )
+      
+    when DOWN
+      return Model::Coord.new(
+        curr_position.row + 1,
+        curr_position.col
+      )
+
+    when LEFT
+      return Model::Coord.new(
+        curr_position.row,
+        curr_position.col - 1
+      )
+
+    end
+  end
+
+  def position_is_valid?(state, position)
+    is_invalid = (
+      (position.row >= state.grid.rows || position.row < 0)
+      ||
+      (position.col >= state.grid.cols || position.col < 0)
+    )
+    return false is is_invalid
+
+    return !(state.snake.positions.include? position)
+  end
+
+  def move_snake_to(state, next_position)
+    next_positions = [next_position] + state.snake.positions[0...-1]
+    state.snake.positions = next_positions
+    state
+  end
+
+  def end_game(state)
+    state.game_finished = true
+    state
+  end
+end
