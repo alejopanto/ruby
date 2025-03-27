679ffb8d51fb9daf0135d23e5c39219546ca5c40
alejopanto <jose.pantoja@testu.co>
2025-03-27 11:46:54 -0500

    cambiar direcion

--- Course_ruby/snake/src/actions/actions.rb
+++ Course_ruby/snake/src/actions/actions.rb
@@ -1,6 +1,6 @@
 module Actions
   def self.move_snake(state)
-    next_direction = state.next_direction
+    next_direction = state.curr_direction
     next_position = calc_next_position(state)
 
     if position_is_valid?(state, next_position)
@@ -10,13 +10,22 @@ module Actions
     end
   end
 
+  def self.change_direction(state, direction)
+    if next_direction_is_valid?(state, direction)
+      state.curr_direction = direction
+    else
+      puts "invalid direction"
+    end
+    state
+  end
+
   private
 
   def self.calc_next_position(state)
 
     curr_position = state.snake.position.first
 
-    case state.next_direction
+    case state.curr_direction
     when Model::Direction::UP
       return Model::Coord.new(
         curr_position.row - 1,
@@ -64,5 +73,20 @@ module Actions
   def self.end_game(state)
     state.game_finished = true
     state
+  end
+
+  def self.next_direction_is_valid(state, direction)
+    case state.curr_direction
+    when Model::Direction::UP
+      return true if direction != Model::Direction::DOWN
+    when Model::Direction::DOWN
+      return true if direction != Model::Direction::UP
+    when Model::Direction::LEFT
+      return true if direction != Model::Direction::RIGHT
+    when Model::Direction::RIGHT
+      return true if direction != Model::Direction::LEFT
+    end
+
+    return false
   end
 end
